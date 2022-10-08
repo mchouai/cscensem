61db9c83aee7d405bbe14e6aee3d5b3c3318302b
Mohamed Chouai <89470936+mchouai@users.noreply.github.com>
2022-10-09 00:48:03 +0100

    Update settings.py

--- recrutement/settings.py
+++ recrutement/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT =  'static'
+STATIC_ROOT =  'BASE_DIR/static'
 STATIC_URL = "static/"
 STATICFILES_DIRS = ['./static']
 
