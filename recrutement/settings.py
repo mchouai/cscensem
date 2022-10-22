bf9612adf2bc47d50d89dcc2babd14e812fb5074
mohamed <mohamed.chouai123@gmail.com>
2022-10-22 17:38:34 +0100

    'm'

--- recrutement/settings.py
+++ recrutement/settings.py
@@ -82,6 +82,9 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
