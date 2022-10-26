43e8f893e6a61895e7efd4c2446a37521ac2d964
Mohamed Chouai <89470936+mchouai@users.noreply.github.com>
2022-10-26 11:04:06 +0100

    Update settings.py

--- recrutement/settings.py
+++ recrutement/settings.py
@@ -81,11 +81,7 @@ DATABASES = {
     }
 }
 
-import dj_database_url
 
-db_env = dj_database_url.config(conn_max_age=600)
-
-DATABASES["default"].update(db_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
